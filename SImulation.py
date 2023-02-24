313edb45fc9e368b87e343acb9262f974bb67666
Sebastian <thiagohugo2014@gmail.com>
2023-02-24 12:02:38 -0600

    Creation of the def MemoryUse, and the variables of the environment

--- SImulation.py
+++ SImulation.py
@@ -1,1 +1,15 @@
-print ("Hello word")
+import random
+import simpy
+
+def MemoryUse(env, name, time, RAM, CPU):
+    print("")
+
+
+env = simpy.Environment()
+RAM = simpy.Container(env, init=0, capacity=100)
+CPU= simpy.Resource(env, capacity=1)
+random.seed(10)
+
+for i in range(25):
+    env.process(MemoryUse(env, "Process %s"%i, random.randint(1,10),RAM, CPU))
+env.run(until=100)
