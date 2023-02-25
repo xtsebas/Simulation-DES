0f85a63332900669e8ba5741d0f89ef0d4edaf00
XavierLopez25 <lop22716@uvg.edu.gt>
2023-02-25 14:06:25 -0600

    Actualizacion del manejo de procesos de la memoria RAM, instrucciones por ciclo de reloj e impresion de algunos datos relevantes.

--- SImulation.py
+++ SImulation.py
@@ -2,47 +2,58 @@ import random
 import simpy
 import statistics
 
-def MemoryUse(env, name, time, RAM, CPU):
+
+def MemoryUse(env, name, time, RAM, ramQty, number_Instructions, velocity):
     global totalTime, total
 
+    # Prints the quantity needed for the process
     yield env.timeout(time)
+    print("The %s needs %d quantity of RAM" % (name, ramQty))
     timeProcess = env.now
 
-    timeNeeded = random.randint(1,10)
-    ramNeeded = random.randint(1,10)
+    # Obtain the actual ram space
+    yield RAM.get(ramQty)
+    print("The %s can use the: %d of RAM needed." % (name, ramQty))
 
-    print("The %s starts at %f and needs %d quantity of RAM" % (name, timeNeeded, ramNeeded))
+    neeededInstructions = 0
 
-    with CPU.request() as CPU_, RAM.put(ramNeeded) as RAM_:
-        yield RAM_
-        yield CPU_
-        yield env.timeout(timeNeeded)
-        print("The %s ended at %f" % (name, env.now))
+    # Still uses a complete clock cycle to run all the instructions of the process even if they are less than the stablished number of instructions
+    while neeededInstructions < number_Instructions:
+        with CPU.request() as cpuRequest:
+            yield cpuRequest
 
-    total = env.now - timeProcess
-    print("The %s lasted %f units of time" % (name, total))
-    totalTime += total
-    Elapsed_times.append(total)
+            # Calculates the number of instructions the CPU will do per clock cycle for the process
+            if (number_Instructions - neeededInstructions) >= velocity:
+                new_velocity = velocity
 
+            else:
+                new_velocity = (number_Instructions - neeededInstructions)
 
+            print("The %s will do %d instructions in the CPU." % (name, new_velocity))
+            yield env.timeout(new_velocity/velocity)
+
+            neeededInstructions += new_velocity
+            print("The %s has completed %d of %f instructions" % (name, neeededInstructions, number_Instructions))
+
+        waiting_or_ready = random.randint(1,2)
+
+        #Pending to code the waiting list with a new CPU
 
 
 env = simpy.Environment()
 RAM = simpy.Container(env, init=0, capacity=100)
-CPU= simpy.Resource(env, capacity=1)
+CPU = simpy.Resource(env, capacity=1)
 random.seed(10)
 
 totalTime = 0
-total=0
-Elapsed_times=[]
+total = 0
+elapsed_times = []
 
 for i in range(200):
-    env.process(MemoryUse(env, "Process %s"%i, random.expovariate(1.0/5.0),RAM, CPU))
+    env.process(MemoryUse(env, "Process %s" % i, random.expovariate(1.0 / 5.0), RAM, CPU))
 
 env.run(100)
+standard_deviation = statistics.stdev(elapsed_times)
 
-standard_deviation = statistics.stdev(Elapsed_times)
-
-print("The average time of the processes is: ", totalTime/25.0)
+print("The average time of the processes is: ", totalTime / 25.0)
 print("The standard deviation of the processes is: ", standard_deviation)
-
